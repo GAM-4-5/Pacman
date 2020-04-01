6c371086d0e75e2b897278bacd69856d3afd6284
KarlaLaginja <karla.laginja@skole.hr>
2020-04-01 20:05:23 +0200

    Initial commit

---
+++ Assets/PacmanMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float brzina = 0f;
+	private bool m_FacingRight = true;
+	private bool m_FacingUp = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 x = new Vector3(Input.GetAxis ("Horizontal") * brzina * Time.deltaTime, Input.GetAxis ("Vertical") * brzina * Time.deltaTime, 0);
+		rb.velocity = x;
+
+		if (Input.GetAxis ("Vertical") > 0 && !m_FacingUp) {
+			FlipUp ();
+		} else if (Input.GetAxis ("Vertical") < 0 && m_FacingUp) {
+			FlipUp ();
+		}
+
+	
+		if (Input.GetAxis ("Horizontal") > 0 && !m_FacingRight) {
+			Flip ();
+		}
+	
+		else if (Input.GetAxis("Horizontal") < 0 && m_FacingRight)
+		{
+			Flip();
+		}
+
+	}
+	private void Flip()
+	{
+		m_FacingRight = !m_FacingRight;
+
+		Vector3 y = transform.localScale;
+		y.x *= -1;
+		transform.localScale = y;
+	}
+
+	private void FlipUp()
+	{
+		m_FacingUp = !m_FacingUp;
+
+		Vector3 x = transform.localScale;
+		x.z *= -0.5f;
+		transform.localScale = x;
+	}
+}
---
+++ Assets/pacman.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pacman : MonoBehaviour {
+	
+	public CharacterController2D kretanje;
+
+	public float brzina = 2f;
+
+	float lijevodesno = 0f;
+
+	float goredolje = 0f;
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	public void Update () {
+		lijevodesno = Input.GetAxisRaw("Horizontal") * brzina;
+		goredolje = Input.GetAxisRaw("Vertical") * brzina;
+		
+	}
+	public void FixedUpdate () {
+		kretanje.Move(lijevodesno * Time.fixedDeltaTime, false, false);
+		kretanje.Move1(false, goredolje * Time.fixedDeltaTime, false);
+	}
+
+}
